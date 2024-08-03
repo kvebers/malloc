33a9aad304015ce7629dd7b901b4e98ecc4cb2ff
kvebers <kvebers@student.42heilbronn.de>
2024-08-03 15:12:03 +0200

    Setup of the function to allocate the memory

--- malloc.h
+++ malloc.h
@@ -5,5 +5,23 @@
 # include "libft/libft.h"
 
 void *malloc(size_t size);
+void free(void *ptr);
+void *realloc(void *ptr, size_t size);
+void show_alloc_mem(void);
+
+
+#define GET_MEMORY_SIZE(size, alignment) (((size) + (alignment) - 1) & ~((alignment) - 1)) // Operation (and) and operation (not)
+
+typedef struct chunk {
+    size_t size;
+    struct chunk *next;
+    int free;
+} chunk_t;
+
+
+# define ALIGN_SIZE 128
+# define TINY 256
+# define SMALL 2048
+# define LARGE 4096
 
 #endif
--- src/malloc.c
+++ src/malloc.c
@@ -1,11 +1,40 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <sys/mman.h>
+#include <stdio.h>
+#include "../malloc.h"
 
 
+void *allocateTiny()
+{
+    
+    return NULL;
+}
+
+void *allocateSmall()
+{
+
+    return NULL;
+}
+
+static void *splitBasedOnSizeAndAllocate(size_t size, size_t alignedMemory, int pageSize)
+{
+    void *ptr;
+    (void)pageSize;
+    if (size <= TINY) ptr = allocateTiny();
+    else if (size <= SMALL) ptr = allocateSmall();
+    else ptr = mmap(0, alignedMemory, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0); // read/write and memory mapping flags and file descriptor and offset of memory
+    if (ptr == MAP_FAILED)
+        return NULL;
+    return ptr;
+}
 
 void *malloc(size_t size)
 {
-    printf("malloc %zu\n", size);
-    return NULL;
+    void *ptr;
+    if (size <= 0) return NULL;
+    int pageSize = getpagesize(); // getting the page size of the system
+    size_t alignedMemory = GET_MEMORY_SIZE(size, ALIGN_SIZE); // aligning the memory to improve the performance due to the cache size
+    ptr = splitBasedOnSizeAndAllocate(size, alignedMemory, pageSize);
+    printf("Allocated memory of size %p\n", ptr);
+    return ptr;
 }
