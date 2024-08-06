d4dac1ff2f62d3699b3acd8711fca4eaefe84f6a
kvebers <kvebers@student.42heilbronn.de>
2024-08-06 08:11:37 +0200

    test cases

--- test/test.c
+++ test/test.c
@@ -4,10 +4,10 @@
 int main()
 {
     {
-    void *ptr = malloc(4097);
-    printf("ptr: %p\n", ptr);
-    ptr = malloc(4098);
-    printf("ptr: %p\n", ptr);
+        void *ptr = malloc(4097);
+        printf("ptr: %p\n", ptr);
+        ptr = malloc(4098);
+        printf("ptr: %p\n", ptr);
     }
     {
         printf("=====================================\n");
@@ -26,6 +26,41 @@ int main()
         printf("ptr: %p\n", ptr);
         ptr = malloc(290);
         printf("ptr: %p\n", ptr);
+    }
+    {
+        int		i;
+	    char	*test;
+
+	    i = 0;
+	    while (i < 1024)
+	    {
+		    i++;
+	    }
+    }
+    {
+        int		i;
+	    char	*test;
+
+	    i = 0;
+	    while (i < 1024)
+	    {
+		    test = (char*)malloc(1000);
+		    test[0] = 42;
+		    i++;
+	    }
+    }
+    {
+        int		i;
+        char	*test;
+
+        i = 0;
+        while (i < 1024)
+        {
+            test = (char*)malloc(1000);
+            test[0] = 42;
+            i++;
+            //free
+        }
     }
     {
         printf("=====================================\n");
