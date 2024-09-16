4a97517e9f11196742c52b9facdfce66a71c4278
kvebers <kvebers@student.42heilbronn.de>
2024-09-16 16:42:06 +0200

    Blank commit: xd

--- src/realloc.c
+++ src/realloc.c
@@ -1,7 +1,6 @@
 #include "../malloc.h"
 
 void *realloc(void *ptr, size_t size) {
-    (void)ptr;
-    (void)size;
+    
     return NULL;
 }
--- test/test.c
+++ test/test.c
@@ -5,6 +5,7 @@ int main()
     for (int i = 0; i < 200; i++)
     {
         void *ptr = malloc(1024);
+        free(ptr);
     }
     show_alloc_mem();
     // {
