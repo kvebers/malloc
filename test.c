baa14468ed063cf86c8630a276391953ee6f3176
kvebers <kvebers@student.42heilbronn.de>
2024-09-20 10:53:13 +0200

    Just leave this for the evaluation

--- test.c
+++ test.c
@@ -3,12 +3,12 @@
 
 int main()
 {
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 1024; i++)
     {
-        void *ptr = malloc(2000);
-        char *str = (char *)ptr;
-        ptr = realloc(ptr, 1000);
-        free(ptr);
+        //void *ptr = malloc(1000);
+        //char *str = (char *)ptr;
+        //ptr = realloc(ptr, 1000);
+        //free(ptr);
     }
     show_alloc_mem();
 }
